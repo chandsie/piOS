0770748b231d642a1d866b1bcebbf99a29708ed0
Shreyas Chand <shreyas.chand@gmail.com>
2019-01-18 01:28:07 -0800

    Turn on ACT LED on start

---
+++ include/kernel.h
@@ -0,0 +1,7 @@
+enum {
+    IO_BASE       = 0x20000000,
+    GPIO_BASE     = (IO_BASE + 0x00200000),
+    MAIL_BASE     = (IO_BASE + 0xB880),
+
+    BUS_ADDR_BASE = 0x4000000,
+};
--- source/kernel.c
+++ source/kernel.c
@@ -1,9 +1,24 @@
 #include <stdint.h>
+#include "kernel.h"
+inline void GPIO16Init() {
+    // Set GPIO Ping 16 to output mode
+    volatile uint32_t* fsel = (volatile uint32_t*)(GPIO_BASE + 0x4);
+    *fsel = (1 << 18);
+}
+
+inline void GPIO16High() {
+    volatile uint32_t* gpio_out_clear = (volatile uint32_t*)(GPIO_BASE + 0x28);
+    *gpio_out_clear = (1 << 16);
+}
 
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     // Unused for now
     (void) r0;
     (void) r1;
     (void) atags;
+
+    GPIO16Init();
+    GPIO16High();
+
     return;
 }
