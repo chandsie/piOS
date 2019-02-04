84e33ced8a7a2fa19ccccbf374bb4222f2bcd19c
Shreyas Chand <shreyas.chand@gmail.com>
2019-02-04 00:07:24 -0800

    Fix bus address base and reformat for clarity

--- include/kernel.h
+++ include/kernel.h
@@ -1,12 +1,12 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+// ENSURE ALL CONSTANTS ARE LINED UP AND THERE NO EMPTY LINES
 enum {
-    IO_BASE       = 0x20000000,
+    IO_BASE       =            0x20000000,
     GPIO_BASE     = (IO_BASE + 0x00200000),
-    MAIL_BASE     = (IO_BASE + 0xB880),
-
-    BUS_ADDR_BASE = 0x4000000,
+    MAIL_BASE     = (IO_BASE + 0x0000B880),
+    BUS_ADDR_BASE =            0x40000000,
 };
 
 #endif // KERNEL_H
