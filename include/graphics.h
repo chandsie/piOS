7b08ea74f9cc7e3a2d2af9826b05aac40e000ec6
Shreyas Chand <shreyas.chand@gmail.com>
2019-02-05 23:35:00 -0800

    Add putc function

--- include/graphics.h
+++ include/graphics.h
@@ -9,6 +9,13 @@
 #define PIXEL_BYTES (COLORDEPTH / 8)
 
 typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} pixel_t;
+
+
+typedef struct {
     uint32_t width;
     uint32_t height;
     uint32_t virtual_width;
@@ -22,13 +29,23 @@ typedef struct {
 } fb_info_t;
 
 typedef struct {
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-} pixel_t;
+    uint32_t row_index;
+    uint32_t column_index;
+    const pixel_t * background;
+    const pixel_t * foreground;
+} graphics_info_t;
+
+#define CHAR_HEIGHT   16
+#define GLYPH_WIDTH    8
+#define CHAR_TRACKING  2
+#define CHAR_WIDTH     (CHAR_TRACKING + GLYPH_WIDTH)
+#define LINE_HEIGHT    (CHAR_HEIGHT + 2)
 
 void framebufferInit();
 void clearFrame();
-void drawPixel(uint32_t x, uint32_t y, const pixel_t * pixel);
+void drawPixel(int x, int y, const pixel_t * pixel);
+void puts(const char * str);
+void putc(char c);
+const uint8_t * getCharBitmap(char c);
 
 #endif // GRAPHICS_H
