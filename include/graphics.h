1ea1157c44baffbf5f75c0540b2f19a1c09cc5b8
Shreyas Chand <shreyas.chand@gmail.com>
2019-02-06 21:38:47 -0800

    Refactor out font

--- include/graphics.h
+++ include/graphics.h
@@ -2,6 +2,7 @@
 #define GRAPHICS_H
 
 #include <stdint.h>
+#include <font.h>
 #include <kernel.h>
 #include <mailbox.h>
 
@@ -35,17 +36,10 @@ typedef struct {
     const pixel_t * foreground;
 } graphics_info_t;
 
-#define CHAR_HEIGHT   16
-#define GLYPH_WIDTH    8
-#define CHAR_TRACKING  2
-#define CHAR_WIDTH     (CHAR_TRACKING + GLYPH_WIDTH)
-#define LINE_HEIGHT    (CHAR_HEIGHT + 2)
-
 void framebufferInit();
 void clearFrame();
 void drawPixel(int x, int y, const pixel_t * pixel);
 void puts(const char * str);
 void putc(char c);
-const uint8_t * getCharBitmap(char c);
 
 #endif // GRAPHICS_H
